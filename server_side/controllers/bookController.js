b67b638c770f1434f1b200d8973ccc96263030c8
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-09-28 12:04:14 -0400

    feat: user prompt for summary completion

--- server_side/controllers/bookController.js
+++ server_side/controllers/bookController.js
@@ -11,12 +11,15 @@ const openAIClient = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
 
-const getSumCompletion = async () => {
+const getSumCompletion = async (bookName, pageNumber) => {
   const chatCompletion = await openAIClient.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
       { role: "system", content: systemPromptSum },
-      { role: "user", content: "Can you say hello to me?" },
+      {
+        role: "user",
+        content: `I have been reading the book ${bookName} up to page ${pageNumber} and i have been busy so i have not been able to continue reading in a while. I feel i have forgotten all the little intricacies of the story. can you give me a summary so that I can continue reading from that page without missing anything and feel immersed in the story once again?. can you include sub points like ,a main overview, and key events leading up to the page. can you leave out any comments like "sure here\'s your summary" or "let me know if you need any clarifications? can you also make sure it\'s at least 500 words long?`,
+      },
     ],
   });
 
@@ -61,7 +64,7 @@ const getBookById = async (req, res) => {
 
 const addBook = async (req, res) => {
   const book = req.body;
-  book.description = await getSumCompletion();
+  book.description = await getSumCompletion(book.bookName, book.pageNumber);
 
   try {
     const bookSummary = await Book.create(book);
