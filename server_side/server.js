241128422c9d340b7b4d699739fbf578ff027645
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-09-27 20:26:24 -0400

    feat: init express, mongoose

---
+++ server_side/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.4gnrv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    )
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Node API app is running on http://localhost:${PORT}`);
+        });
+        console.log("connected to mongoDB");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
